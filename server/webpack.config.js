b4f136ffd06e5b7bd09ea3b5ebd8099794f37678
an <tangcossin@gmail.com>
2021-02-16 14:32:11 +0300

    update build packages

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  mode: 'production',
+  target: 'node',
+  externals: [nodeExternals()],
   entry: path.resolve(__dirname + '/index.js'),
   output: {
     path: path.resolve(__dirname + '/build_server'),
